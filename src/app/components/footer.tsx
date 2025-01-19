4f3280f447cff15e8e4ade60a2055d0e1e6de39b
Mustabshira khalid <160775326+Nadeem-59@users.noreply.github.com>
2025-01-19 00:17:25 -0800

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -93,7 +93,7 @@ export default function footer() {
                 {/* Bottom Section */}
                 <section className="bg-[#FAFAFA] py-4">
                     <div className="max-w-screen-xl mx-auto flex justify-between items-center">
-                        <p className="text-[#727272] text-sm font-bold">Made with love by Ashna. All rights reserved.</p>
+                        <p className="text-[#727272] text-sm font-bold">Made with love by Mustabshira Khalid. All rights reserved.</p>
                         <div className="flex gap-4">
                            
                         </div>
